57ac8781da39ccc000a8b3589f85abfe10f83dce
shikurassefa <jabirshikur@gmail.com>
2023-05-08 06:56:29 -0700

    updating a URL

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const tab = require('./data')
+const express = require("express");
+const app = express();
+const tab = require("./data");
 
 const cors = require("cors");
 app.use(cors({ origin: "*" }));
-app.get("api/tab", (req, res) => {
+app.get("/api/tab", (req, res) => {
   res.json(tab);
 });
 const port = process.env.PORT || 3002;
-app.listen(port,()=>console.log('server is on port 5000'))
+app.listen(port, () => console.log("server is on port 5000"));
